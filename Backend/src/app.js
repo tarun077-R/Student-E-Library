93a72de119727957f08f985c5a53af07efc11ecc
tarun077 <tarunrawat8350@gmail.com>
2026-03-12 16:41:28 +0530

    fix cors origin for netlify

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,23 +1,30 @@
 const express = require("express")
 const cors = require('cors')
 const cookieParser = require('cookie-parser');
+
 const authRoutes = require('./routes/authRoutes')
 const bookRoutes = require('./routes/bookRoutes')
-const userRoutes=require("./routes/userRoutes")
-const app= express()
-app.use(express.json())
-app.use(cookieParser());
-app.use(cors({
-    origin:['http://localhost:5173',
-        "https://student-e-library.netlify.app/"],
+const userRoutes = require("./routes/userRoutes")
 
-    credentials:true
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://student-e-library.netlify.app"
+  ],
+  credentials: true
 }))
+
 app.get("/", (req, res) => {
   res.send("Student E-Library API is running 🚀");
-});
+})
 
-app.use('/api/auth',authRoutes)
-app.use("/api/books",bookRoutes)
-app.use("/api/users",userRoutes)
+app.use('/api/auth', authRoutes)
+app.use("/api/books", bookRoutes)
+app.use("/api/users", userRoutes)
+
 module.exports = app
