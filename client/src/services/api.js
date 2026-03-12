b2c868436f1a7c65eba027cb4dd862663f16d4c4
tarun077 <tarunrawat8350@gmail.com>
2026-03-12 16:14:07 +0530

    change baseUrl route

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'http://student-e-library.onrender.com/api',
     withCredentials: true  // cookies ke liye zaroori hai
 })
 
