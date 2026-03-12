c1c372a5d6ed986bd6b4228c747faaecc679e93a
tarun077 <tarunrawat8350@gmail.com>
2026-03-12 16:08:32 +0530

    add root route

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,6 +11,9 @@ app.use(cors({
     origin:'http://localhost:5173',
     credentials:true
 }))
+app.get("/", (req, res) => {
+  res.send("Student E-Library API is running 🚀");
+});
 
 app.use('/api/auth',authRoutes)
 app.use("/api/books",bookRoutes)
